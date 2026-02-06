07092a150b0b80004d100360ce0c55fed25224d8
Matheus Pompeo <matheuspompeo13@gmail.com>
2026-02-06 00:28:23 -0300

    feat: adiciona DTOs com validações

---
+++ InvestAPI/DTOs/Users/UserResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace InvestAPI.DTOs.Users
+{
+    public class UserResponseDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+    }
+}
